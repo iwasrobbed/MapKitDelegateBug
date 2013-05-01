958b69aa3a6359769e185009aa6e8c559b68cc77
Rob Phillips <ufbobbo@gmail.com>
2013-05-01 11:00:43 -0400

    Initial Commit

---
+++ MapKitDelegateBug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapKitDelegateBug
+//
+//  Created by Rob Phillips on 5/1/13.
+//  Copyright (c) 2013 Rob Phillips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapKitDelegateBug/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapKitDelegateBug
+//
+//  Created by Rob Phillips on 5/1/13.
+//  Copyright (c) 2013 Rob Phillips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
